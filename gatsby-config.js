4a296410c617bd9c16a1bb09b7b2a04f26988e36
twilightscapes <ihatespam@gmail.com>
2021-08-07 14:30:53 -0500

    fix break

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
       options: {
         // A selector to set react-modal's app root to, default is `#___gatsby`
         // See http://reactcommunity.org/react-modal/accessibility/#app-element
-        appElement: '#___adfree',
+        appElement: '#___gatsby',
 
         // Object of props that will be passed to the react-modal container
         // See http://reactcommunity.org/react-modal/#usage
